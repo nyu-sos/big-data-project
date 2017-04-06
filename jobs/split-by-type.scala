c9002ee474813f96c039837fccb63e0f47bae7f3
Willian Z <Willian@Willian-Zhang.com>
2017-04-06 18:54:03 -0400

    Update exec method

--- jobs/split-by-type.scala
+++ jobs/split-by-type.scala
@@ -1,34 +1,23 @@
-/* SimpleApp.scala */
 //import org.apache.spark.SparkContext
 //import org.apache.spark.SparkContext._
 //import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+//import org.apache.spark.sql.SparkSession
 
-object sosSplit {
-  def main(args: Array[String]) {
-    val dataFile = "sos-311.csv";
+val dataFile = "sos-311.csv";
 
-    val spark = SparkSession
-        .builder()
-        .appName("SOS - 311 - Split")
-        .getOrCreate();
+//val spark = SparkSession.builder().appName("SOS - 311 - Split").getOrCreate();
 
-    val df = spark.read
-        .format("com.databricks.spark.csv")
-        .option("header", "true") 
-        .load(dataFile);
+val df = spark.read.format("com.databricks.spark.csv").option("header", "true").load(dataFile);
 
-    // df.printSchema()
-    
-    val agencies = df.select("Agency")
-        .distinct.collect.flatMap(_.toSeq);
+// df.printSchema()
 
-    val byAgencyDFs = agencies.map(
-      agency => ( agency ->
-          df.where($"Agency" <=> agency)
-            .write.format("csv").save(s"project/Agency-$agency.csv")
-        )
-    ).toMap;
+val agencies = df.select("Agency").distinct.collect.flatMap(_.toSeq);
 
-  }
-}
+val byAgencyDFs = agencies.map(
+    agency => ( agency ->
+        df.where($"Agency" <=> agency)
+        .write.format("csv").save(s"project/Agency-$agency.csv")
+    )
+).toMap;
+
+System.exit(0)
