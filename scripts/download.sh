effb10d7aecddc133687b0ca889807ceb188c8a8
Willian Z <Willian@Willian-Zhang.com>
2017-04-06 13:20:37 -0400

    Update download address

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,5 +2,6 @@
 
 mkdir -p data
 
-wget https://data.cityofnewyork.us/api/views/9s88-aed8/rows.csv?accessType=DOWNLOAD -O data/311.2009.csv
-wget https://data.cityofnewyork.us/api/views/wpe2-h2i5/rows.csv?accessType=DOWNLOAD -O data/311.2010-2016.csv
+# wget https://data.cityofnewyork.us/api/views/9s88-aed8/rows.csv?accessType=DOWNLOAD -O data/311.2009.csv
+# wget https://data.cityofnewyork.us/api/views/wpe2-h2i5/rows.csv?accessType=DOWNLOAD -O data/311.2010-2016.csv
+wget https://data.cityofnewyork.us/api/views/p6vy-ufwu/rows.csv?accessType=DOWNLOAD -O data/311.csv
