9be4821f7693664380ac0e72eec4c366c1facf7d
Willian Z <Willian@Willian-Zhang.com>
2017-04-06 02:35:51 -0400

    Fix download link

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data
 
-wget https://data.cityofnewyork.us/api/views/3rfa-3xsf/rows.csv?accessType=DOWNLOAD -O data/311.2009.csv
-wget https://data.cityofnewyork.us/api/views/erm2-nwe9/rows.csv?accessType=DOWNLOAD -O data/311.2010-2016.csv
+wget https://data.cityofnewyork.us/api/views/9s88-aed8/rows.csv?accessType=DOWNLOAD -O data/311.2009.csv
+wget https://data.cityofnewyork.us/api/views/wpe2-h2i5/rows.csv?accessType=DOWNLOAD -O data/311.2010-2016.csv
