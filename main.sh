c2d63ef45b8e61605edd3225bff3b5e3e571cfbc
Willian Z <Willian@Willian-Zhang.com>
2017-04-06 21:29:57 -0400

    Update grape file

--- main.sh
+++ main.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+projectDir="project/"
 ./scripts/setup_env.sh
 hfs -put ./data/311.csv sos-311.csv
+./scripts/split.sh
+lines=$(hfs -ls -d $projectDir/Agency-*.csv | awk '{print $8}')
+for file in $lines; do
+    hfs -getmerge $file $file
+done
