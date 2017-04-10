cd4686ae1b854ee259856b0db72b0e00c2009480
Willian Z <Willian@Willian-Zhang.com>
2017-04-10 15:55:56 -0400

    Merge shell scripting env

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 projectDir="project/"
-./scripts/setup-env.sh
+. ./scripts/setup-env.sh
 /usr/bin/hadoop fs -put ./data/311.csv sos-311.csv
 ./scripts/split.sh
 lines=$(/usr/bin/hadoop fs -ls -d $projectDir/Agency-*.csv | awk '{print $8}')
