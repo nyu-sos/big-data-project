ea853d3766bdcddec24d79a52587b78f50cb878b
Willian Z <Willian@Willian-Zhang.com>
2017-04-10 15:57:54 -0400

    Revert #7f178c2

--- main.sh
+++ main.sh
@@ -2,11 +2,11 @@
 
 projectDir="project/"
 . ./scripts/setup-env.sh
-/usr/bin/hadoop fs -put ./data/311.csv sos-311.csv
+hfs -put ./data/311.csv sos-311.csv
 ./scripts/split.sh
-lines=$(/usr/bin/hadoop fs -ls -d $projectDir/Agency-*.csv | awk '{print $8}')
+lines=$(hfs -ls -d $projectDir/Agency-*.csv | awk '{print $8}')
 for file in $lines; do
-    /usr/bin/hadoop fs -getmerge $file data/$file
+    hfs -getmerge $file data/$file
 done
 mv data/$projectDir/Agency-*.csv data/Agency/
 rm -r data/$projectDir
