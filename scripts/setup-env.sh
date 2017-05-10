3b295b1c6d39a90d9192f706591da866bbf8e2d9
Willian Z <Willian@Willian-Zhang.com>
2017-05-09 23:26:45 -0400

    Fix scala jar version

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -4,8 +4,8 @@ alias hfs='/usr/bin/hadoop fs '
 spark2 (){
   if [[ $# = 0 ]]
   then
-    spark2-shell --jars /opt/cloudera/parcels/CDH/jars/commons-lang3-3.1.jar --conf spark.driver.extraJavaOptions="-Dscala.color" --conf spark.yarn.user.classpath.first=true
+    spark2-shell --jars /opt/cloudera/parcels/CDH/jars/commons-lang3-3.3.2.jar --conf spark.driver.extraJavaOptions="-Dscala.color" --conf spark.yarn.user.classpath.first=true
   else
-    spark2-shell --jars /opt/cloudera/parcels/CDH/jars/commons-lang3-3.1.jar --conf spark.driver.extraJavaOptions="-Dscala.color" --conf spark.yarn.user.classpath.first=true --conf spark.ui.port=$1
+    spark2-shell --jars /opt/cloudera/parcels/CDH/jars/commons-lang3-3.3.2.jar --conf spark.driver.extraJavaOptions="-Dscala.color" --conf spark.yarn.user.classpath.first=true --conf spark.ui.port=$1
   fi
 }
